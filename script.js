f0a5233eaff341b8337d37eb2f1362ec6d9589f0
Coding With Sourav <so34ur56@gmail.com>
2024-11-08 09:48:32 +0530

    adding files

---
+++ script.js
@@ -0,0 +1,388 @@
+const myValue = document.getElementById("myValue");
+const comValue = document.getElementById("comValue");
+const stone = document.getElementById("stone");
+const paper = document.getElementById("paper");
+const scissors = document.getElementById("scissors");
+const whoWin = document.getElementById("whoWin");
+const score = document.getElementById("score");
+const restart = document.getElementById("restart");
+score.innerText = 0;
+
+//Getting values for Computer By using Math.random()
+function comChoose() {
+    let comChoose = Math.random()
+    if (comChoose < 0.1) {
+        comValue.innerText = paper.innerText
+    }
+    else if (comChoose < 0.2) {
+        comValue.innerText = scissors.innerText
+    }
+
+    else if (comChoose < 0.3) {
+        comValue.innerText = stone.innerText
+    }
+
+    else if (comChoose < 0.4) {
+        comValue.innerText = paper.innerText
+    }
+    else if (comChoose < 0.5) {
+        comValue.innerText = scissors.innerText
+    }
+    else if (comChoose < 0.6) {
+        comValue.innerText = stone.innerText
+    }
+    else if (comChoose < 0.7) {
+        comValue.innerText = scissors.innerText
+    }
+    else if (comChoose < 0.8) {
+        comValue.innerText = paper.innerText
+    }
+
+    else if (comChoose < 0.9) {
+        comValue.innerText = stone.innerText
+    }
+    else if (comChoose < 0.10) {
+        comValue.innerText = scissors.innerText
+    }
+    else if (comChoose < 0.11) {
+        comValue.innerText = paper.innerText
+    }
+
+    else if (comChoose < 0.12) {
+        comValue.innerText = scissors.innerText
+    }
+    else if (comChoose < 0.13) {
+        comValue.innerText = stone.innerText
+    }
+    else if (comChoose < 0.14) {
+        comValue.innerText = paper.innerText
+    }
+    else if (comChoose < 0.15) {
+        comValue.innerText = stone.innerText
+    }
+    else if (comChoose < 0.16) {
+        comValue.innerText = paper.innerText
+    }
+    else if (comChoose < 0.17) {
+        comValue.innerText = stone.innerText
+    }
+
+    else if (comChoose < 0.18) {
+        comValue.innerText = scissors.innerText
+    }
+    else if (comChoose < 0.19) {
+        comValue.innerText = paper.innerText
+    }
+    else if (comChoose < 0.20) {
+        comValue.innerText = scissors.innerText
+    }
+    else if (comChoose < 0.21) {
+        comValue.innerText = stone.innerText
+    }
+    else if (comChoose < 0.22) {
+        comValue.innerText = scissors.innerText
+    }
+    else if (comChoose < 0.23) {
+        comValue.innerText = paper.innerText
+    }
+    else if (comChoose < 0.24) {
+        comValue.innerText = stone.innerText
+    }
+    else if (comChoose < 0.25) {
+        comValue.innerText = scissors.innerText
+    }
+    else if (comChoose < 0.26) {
+        comValue.innerText = paper.innerText
+    }
+    else if (comChoose < 0.27) {
+        comValue.innerText = scissors.innerText
+    }
+    else if (comChoose < 0.28) {
+        comValue.innerText = stone.innerText
+    }
+    else if (comChoose < 0.29) {
+        comValue.innerText = paper.innerText
+    }
+    else if (comChoose < 0.30) {
+        comValue.innerText = scissors.innerText
+    }
+    else if (comChoose < 0.31) {
+        comValue.innerText = stone.innerText
+    }
+    else if (comChoose < 0.32) {
+        comValue.innerText = scissors.innerText
+    }
+    else if (comChoose < 0.33) {
+        comValue.innerText = paper.innerText
+    }
+    else if (comChoose < 0.34) {
+        comValue.innerText = scissors.innerText
+    }
+    else if (comChoose < 0.35) {
+        comValue.innerText = paper.innerText
+    }
+    else if (comChoose < 0.36) {
+        comValue.innerText = stone.innerText
+    }
+    else if (comChoose < 0.37) {
+        comValue.innerText = scissors.innerText
+    }
+
+    else if (comChoose < 0.38) {
+        comValue.innerText = stone.innerText
+    }
+    else if (comChoose < 0.39) {
+        comValue.innerText = paper.innerText
+    }
+    else if (comChoose < 0.40) {
+        comValue.innerText = scissors.innerText
+    }
+    else if (comChoose < 0.41) {
+        comValue.innerText = paper.innerText
+    }
+    else if (comChoose < 0.42) {
+        comValue.innerText = stone.innerText
+    }
+    else if (comChoose < 0.43) {
+        comValue.innerText = scissors.innerText
+    }
+    else if (comChoose < 0.44) {
+        comValue.innerText = paper.innerText
+    }
+    else if (comChoose < 0.45) {
+        comValue.innerText = scissors.innerText
+    }
+    else if (comChoose < 0.46) {
+        comValue.innerText = stone.innerText
+    }
+    else if (comChoose < 0.47) {
+        comValue.innerText = paper.innerText
+    }
+    else if (comChoose < 0.48) {
+        comValue.innerText = scissors.innerText
+    }
+    else if (comChoose < 0.49) {
+        comValue.innerText = stone.innerText
+    }
+    else if (comChoose < 0.50) {
+        comValue.innerText = paper.innerText
+    }
+    else if (comChoose < 0.51) {
+        comValue.innerText = scissors.innerText
+    }
+    else if (comChoose < 0.52) {
+        comValue.innerText = stone.innerText
+    }
+    else if (comChoose < 0.53) {
+        comValue.innerText = paper.innerText
+    }
+    else if (comChoose < 0.54) {
+        comValue.innerText = scissors.innerText
+    }
+    else if (comChoose < 0.55) {
+        comValue.innerText = stone.innerText
+    }
+    else if (comChoose < 0.56) {
+        comValue.innerText = paper.innerText
+    }
+    else if (comChoose < 0.57) {
+        comValue.innerText = scissors.innerText
+    }
+    else if (comChoose < 0.58) {
+        comValue.innerText = stone.innerText
+    }
+    else if (comChoose < 0.59) {
+        comValue.innerText = paper.innerText
+    }
+    else if (comChoose < 0.60) {
+        comValue.innerText = scissors.innerText
+    }
+    else if (comChoose < 0.61) {
+        comValue.innerText = stone.innerText
+    }
+    else if (comChoose < 0.62) {
+        comValue.innerText = paper.innerText
+    }
+
+    else if (comChoose < 0.63) {
+        comValue.innerText = scissors.innerText
+    }
+    else if (comChoose < 0.64) {
+        comValue.innerText = stone.innerText
+    }
+    else if (comChoose < 0.65) {
+        comValue.innerText = paper.innerText
+    }
+    else if (comChoose < 0.66) {
+        comValue.innerText = scissors.innerText
+    }
+    else if (comChoose < 0.67) {
+        comValue.innerText = stone.innerText
+    }
+    else if (comChoose < 0.68) {
+        comValue.innerText = paper.innerText
+    }
+    else if (comChoose < 0.69) {
+        comValue.innerText = stone.innerText
+    }
+    else if (comChoose < 0.70) {
+        comValue.innerText = scissors.innerText
+    }
+    else if (comChoose < 0.71) {
+        comValue.innerText = paper.innerText
+    }
+
+    else if (comChoose < 0.72) {
+        comValue.innerText = paper.innerText
+    }
+    else if (comChoose < 0.73) {
+        comValue.innerText = scissors.innerText
+    }
+    else if (comChoose < 0.74) {
+        comValue.innerText = stone.innerText
+    }
+    else if (comChoose < 0.75) {
+        comValue.innerText = paper.innerText
+    }
+    else if (comChoose < 0.76) {
+        comValue.innerText = scissors.innerText
+    }
+    else if (comChoose < 0.77) {
+        comValue.innerText = paper.innerText
+    }
+    else if (comChoose < 0.78) {
+        comValue.innerText = stone.innerText
+    }
+    else if (comChoose < 0.79) {
+        comValue.innerText = scissors.innerText
+    }
+
+    else if (comChoose < 0.80) {
+        comValue.innerText = paper.innerText
+    }
+    else if (comChoose < 0.81) {
+        comValue.innerText = scissors.innerText
+    }
+
+    else if (comChoose < 0.82) {
+        comValue.innerText = paper.innerText
+    }
+    else if (comChoose < 0.83) {
+        comValue.innerText = stone.innerText
+    }
+    else if (comChoose < 0.84) {
+        comValue.innerText = paper.innerText
+    }
+    else if (comChoose < 0.85) {
+        comValue.innerText = scissors.innerText
+    }
+    else if (comChoose < 0.86) {
+        comValue.innerText = stone.innerText
+    }
+    else if (comChoose < 0.87) {
+        comValue.innerText = paper.innerText
+    }
+    else if (comChoose < 0.88) {
+        comValue.innerText = scissors.innerText
+    }
+    else if (comChoose < 0.89) {
+        comValue.innerText = stone.innerText
+    }
+    else if (comChoose < 0.90) {
+        comValue.innerText = paper.innerText
+    }
+    else if (comChoose < 0.91) {
+        comValue.innerText = scissors.innerText
+    }
+    else if (comChoose < 0.92) {
+        comValue.innerText = paper.innerText
+    }
+    else if (comChoose < 0.93) {
+        comValue.innerText = stone.innerText
+    }
+    else if (comChoose < 0.94) {
+        comValue.innerText = scissors.innerText
+    }
+    else if (comChoose < 0.95) {
+        comValue.innerText = paper.innerText
+    }
+    else if (comChoose < 0.96) {
+        comValue.innerText = stone.innerText
+    }
+    else if (comChoose < 0.97) {
+        comValue.innerText = paper.innerText
+    }
+    else if (comChoose < 0.98) {
+        comValue.innerText = scissors.innerText
+    }
+    else if (comChoose < 0.99) {
+        comValue.innerText = stone.innerText
+    }
+    else {
+        comValue.innerText = scissors.innerText
+    }
+}
+
+//Getting Conditions for Win, Lost and Draw
+function result() {
+    if ((myValue.innerText === stone.innerText) && (comValue.innerText === scissors.innerText) || (myValue.innerText === scissors.innerText) && (comValue.innerText === paper.innerText) || (myValue.innerText === paper.innerText) && (comValue.innerText === stone.innerText)) {
+        whoWin.innerText = "You Won"
+    }
+
+    else if ((myValue.innerText === paper.innerText) && (comValue.innerText === paper.innerText) || (myValue.innerText === stone.innerText) && (comValue.innerText === stone.innerText) || (myValue.innerText === scissors.innerText) && (comValue.innerText === scissors.innerText)) {
+        whoWin.innerText = "Draw"
+    }
+
+    else{
+         whoWin.innerText = "You Lost"
+    }
+}
+
+
+//Getting points for Win, Lose and Draw
+function scoreValue() {
+
+    if (whoWin.innerText === "You Won") {
+        score.innerText++;
+    }
+
+    else if (whoWin.innerText === "Draw") {
+        score.innerText = score.innerText;
+    }
+
+    else if (whoWin.innerText === "You Lost") {
+        score.innerText--;
+    }
+
+}
+
+stone.addEventListener("click", async function () {
+
+    myValue.innerText = stone.innerText;
+    await comChoose();
+    await result();
+    await scoreValue();
+})
+
+scissors.addEventListener("click", async function () {
+
+    myValue.innerText = scissors.innerText;
+    await comChoose();
+    await result();
+    await scoreValue();
+})
+
+paper.addEventListener("click", async function () {
+
+    myValue.innerText = paper.innerText;
+    await comChoose();
+    await result();
+    await scoreValue();
+})
+
+restart.addEventListener("click", () => {
+    myValue.innerText = "";
+    comValue.innerText = "";
+    score.innerText = 0;
+    whoWin.innerText = ""
+})
